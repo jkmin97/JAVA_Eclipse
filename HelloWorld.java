89b948debce57c1d54d741a8afd4c0886d9c99f5
jkmin97 <99165458+jkmin97@users.noreply.github.com>
2022-03-10 20:57:17 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package org.day01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이름 : 정경민\n나이 : 26살\n사는 곳 : 경기도 수원\n출신대학 : 아주대학교\n개발경력 : 없음\n좋은 강의 부탁드립니다^^");
+	}
+
+}
